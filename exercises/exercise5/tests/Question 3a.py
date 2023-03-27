d8bfdf73ed4fff4d3d26ec1c0b97445fc76ef700
ValentinDebarnot <valentin.debarnot@gmail.com>
2023-03-27 17:30:33 +0200

    add exercise 5

--- exercises/exercise5/tests/Question 3a.py
+++ exercises/exercise5/tests/Question 3a.py
@@ -2,9 +2,9 @@ OK_FORMAT = True
 
 test = {   'name': 'Question 3a',
     'points': 1,
-    'suites': [   {   'cases': [   {'code': '>>> assert_equal(len(x), n_samples)\n', 'hidden': False, 'locked': False},
-                                   {'code': '>>> assert_almost_equal(x.mean(), 2.963 , places=2)\n', 'hidden': False, 'locked': False},
-                                   {'code': '>>> assert_almost_equal(x.std() , 1.975 , places=2)\n', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [   {'code': '>>> assert_equal(len(y), n_samples)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert_almost_equal(y.mean(), 2.963 , places=2)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert_almost_equal(y.std() , 1.975 , places=2)\n', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
