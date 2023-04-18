2dfac34056ff4e38ddfa26c061162fcaef80a53f
AmirEhsan1996 <amirehsan.khorashadizadeh@gmail.com>
2023-04-18 12:57:58 +0200

    exercise 7

---
+++ exercises/exercise7/tests/Question 1a.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'Question 1a',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> assert_equal(toss_coin_all.shape[0],1000)\n', 'hidden': False, 'locked': False},
+                                   {'code': '>>> assert_equal(toss_coin_all.shape[1],100)\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ exercises/exercise7/tests/Question 1b.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'Question 1b',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> assert_equal(toss_sum.shape[0],1000)\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
